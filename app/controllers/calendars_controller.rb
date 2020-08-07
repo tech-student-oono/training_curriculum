0202b4255a47f8ea6458f9daa8f443db3bad1b03
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-07 19:03:58 +0900

    Issue2修正2回目

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,7 +3,7 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
-    @plan = plan.new
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -27,7 +27,7 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    @plans = plan.where(date: @todays_date..@todays_date + 7)
+    @plans = Plan.where(date: @todays_date..@todays_date + 7)
 
     7.times do |x|
       plans = []
